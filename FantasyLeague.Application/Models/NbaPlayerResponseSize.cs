93b1c9f78ae858a66b8107e245e4afa6f023e8de
Umut Sogukpinar <usogukpinar0@gmail.com>
2026-07-23 09:17:06 +0300

    feat: add enums for dtos

---
+++ FantasyLeague.Application/Models/NbaPlayerResponseSize.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FantasyLeague.Application.Models;
+
+public enum PlayerResponseSize
+{
+    Basic,
+    Detailed,
+    Extended
+}
